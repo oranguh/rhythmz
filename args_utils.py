00df9a58f54ceee0235e7eff3ac81fc6c2efc058
samarthbhargav <samarth.bhargav92@gmail.com>
2018-10-09 19:46:03 +0200

    Added script to download the INDIC data

---
+++ args_utils.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description='TODO')
+    parser.add_argument('--verbose-off', dest='verbose_off', required=False,
+                        default=False, action='store_true', help="set to true for verbose output")
+    parser.add_argument('--seed', dest="seed", required=False,
+                        default=42, help="random seed")
+
+    subparsers = parser.add_subparsers(dest='module', help='module to run')
+
+    # subparaser for building permit-desc classifiers
+    parser_process = subparsers.add_parser(
+        "process", help="process audio files")
+    parser_process.add_argument("dataset", type=str, help="the dataset to process")
+    parser_process.add_argument("--input", type=str, required=True, help="location of input data")
+    parser_process.add_argument("--output", type=str, required=True, help="location of the output folder")
+    return parser
+
