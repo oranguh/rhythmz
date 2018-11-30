d09466de77bc6394ae3843a0ed6715532c5e7c23
samarthbhargav <samarth.bhargav92@gmail.com>
2018-11-30 13:42:39 +0100

    logging changes + check module

--- rhythm.py
+++ rhythm.py
@@ -6,12 +6,17 @@ from args_utils import get_argparser
 
 if __name__ == '__main__':
 
-    # todo verbosity
-    logging.basicConfig(level=logging.DEBUG)
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
-    log = logging.getLogger(__name__)
+    log = logging.getLogger("rhythm")
 
     args = get_argparser().parse_args()
+
+    if args.module is None:
+        raise ValueError("Provide a module argument (see --help)")
 
     if args.module == "process":
         data_process = process.Process(args)
