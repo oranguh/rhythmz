6f134848d7918410955348e4b3b9f20e0c2cdeb3
samarthbhargav <samarth.bhargav92@gmail.com>
2019-01-28 13:44:20 +0100

    Added a ref to split_datasets.py

--- split_datasets.py
+++ split_datasets.py
@@ -15,7 +15,6 @@ def mkdir(path):
 
 
 def read_files(folder):
-    # return [file_name.split("/")[-1] for file_name in list_dir(folder)]
     return [os.path.split(file_name)[-1] for file_name in list_dir(folder)]
 
 
@@ -32,6 +31,15 @@ def write_split(path, data):
         shutil.copy(d[1], dest)
 
 
+def read_split(path, split, prefix):
+    path = os.path.join(path, split)
+    files = []
+    for cl in os.listdir(path):
+        for file in os.listdir(os.path.join(os.path.join(path, cl))):
+            files.append((cl, os.path.join(prefix, cl, file)))
+    return files
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
@@ -43,6 +51,8 @@ if __name__ == '__main__':
                         help="location of input folder")
     parser.add_argument("output_folder", type=str,
                         help="location of output folder")
+    parser.add_argument("--ref-folder", dest="ref_folder",
+                        help="if specified, does splits based on this folder structure")
     parser.add_argument("--seed", type=int, help="seed for RNG", required=True)
     parser.add_argument("--train-split", dest="train_split", default=0.6,
                         type=float, help="(0, 1.0) %age of train split")
@@ -53,34 +63,39 @@ if __name__ == '__main__':
 
     np.random.seed(args.seed)
 
-    train_split = args.train_split
-    val_split = args.val_split
-    test_split = 1 - (train_split + val_split)
+    if args.ref_folder:
+        train_split = read_split(args.ref_folder, "train", prefix=args.input_folder)
+        val_split = read_split(args.ref_folder, "val", prefix=args.input_folder)
+        test_split = read_split(args.ref_folder, "test", prefix=args.input_folder)
+    else:
+        train_split = args.train_split
+        val_split = args.val_split
+        test_split = 1 - (train_split + val_split)
 
-    log.info("Train: {}, Val: {}, Test: {}".format(
-        train_split, val_split, test_split))
+        log.info("Train: {}, Val: {}, Test: {}".format(
+            train_split, val_split, test_split))
 
-    mkdir(args.output_folder)
-    mkdir(os.path.join(args.output_folder, "train"))
-    mkdir(os.path.join(args.output_folder, "test"))
-    mkdir(os.path.join(args.output_folder, "val"))
+        mkdir(args.output_folder)
+        mkdir(os.path.join(args.output_folder, "train"))
+        mkdir(os.path.join(args.output_folder, "test"))
+        mkdir(os.path.join(args.output_folder, "val"))
 
-    all_files = []
-    for class_name in os.listdir(args.input_folder):
-        class_path = os.path.join(args.input_folder, class_name)
-        class_files = [os.path.join(class_path, _)
-                       for _ in os.listdir(class_path)]
-        log.info("Class: {}. Instances found: {}".format(
-            class_name, len(class_files)))
-        all_files.extend([(class_name, cf) for cf in class_files])
+        all_files = []
+        for class_name in os.listdir(args.input_folder):
+            class_path = os.path.join(args.input_folder, class_name)
+            class_files = [os.path.join(class_path, _)
+                           for _ in os.listdir(class_path)]
+            log.info("Class: {}. Instances found: {}".format(
+                class_name, len(class_files)))
+            all_files.extend([(class_name, cf) for cf in class_files])
 
-    train_idx = int(train_split * len(all_files))
-    val_idx = int(val_split * len(all_files)) + train_idx
+        train_idx = int(train_split * len(all_files))
+        val_idx = int(val_split * len(all_files)) + train_idx
 
-    all_files_sorted = np.random.permutation(all_files)
-    train_split = all_files_sorted[:train_idx]
-    val_split = all_files_sorted[train_idx: val_idx]
-    test_split = all_files_sorted[val_idx:]
+        all_files_sorted = np.random.permutation(all_files)
+        train_split = all_files_sorted[:train_idx]
+        val_split = all_files_sorted[train_idx: val_idx]
+        test_split = all_files_sorted[val_idx:]
 
     log.info("Train Size: {}, Val Size: {}, Test Size: {}".format(
         len(train_split), len(val_split), len(test_split)))
